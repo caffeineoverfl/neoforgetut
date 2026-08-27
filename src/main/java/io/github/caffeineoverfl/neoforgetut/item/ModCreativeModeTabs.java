1a2a0e32cdcbec023ba4808b941bd9d2ec1ecabb
Toni <tonikrug2000@icloud.com>
2026-08-27 17:22:46 +0200

    Added Black Opal Ore Blocks for stone, deepslate, nether and end

--- src/main/java/io/github/caffeineoverfl/neoforgetut/item/ModCreativeModeTabs.java
+++ src/main/java/io/github/caffeineoverfl/neoforgetut/item/ModCreativeModeTabs.java
@@ -34,6 +34,10 @@ public class ModCreativeModeTabs {
             .displayItems((pParameters, pOutput) -> {
                 pOutput.accept(ModBlocks.BLACK_OPAL_BLOCK);
                 pOutput.accept(ModBlocks.RAW_BLACK_OPAL_BLOCK);
+                pOutput.accept(ModBlocks.BLACK_OPAL_ORE);
+                pOutput.accept(ModBlocks.BLACK_OPAL_END_ORE);
+                pOutput.accept(ModBlocks.BLACK_OPAL_DEEPSLATE_ORE);
+                pOutput.accept(ModBlocks.BLACK_OPAL_NETHER_ORE);
             }).build());
 
     public static void register(IEventBus eventBus){
